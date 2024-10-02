b00c06e051f500658d5a0890a72abd913da8f089
Zagaz <andre_ranulfo@hotmail.com>
2024-10-02 13:35:59 -0300

    Refactor asset versions in build files and Links

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bdfe78153d8bf0bde321');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
+<?php return array('dependencies' => array(), 'version' => 'ba218433c4bfd9590f8f');
